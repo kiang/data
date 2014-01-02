7274dceb7c41ef362faaa1e465b8d5f029ff96fc
kiang <kiange@gmail.com>
2014-01-03 00:53:34 +0800

    try to get all contents

--- app/tasks/MainTask.php
+++ app/tasks/MainTask.php
@@ -4,8 +4,9 @@ class MainTask extends \Phalcon\CLI\Task {
 
     public function mainAction() {
         $cacheFolder = APPLICATION_PATH . '/cache/data.gov.tw/' . date('Y-m-d');
-        if (!file_exists($cacheFolder)) {
-            mkdir($cacheFolder, 0777, true);
+        $dataFolder = APPLICATION_PATH . '/cache/data.gov.tw/' . date('Y-m-d') . '/data';
+        if (!file_exists($dataFolder)) {
+            mkdir($dataFolder, 0777, true);
         }
         $listUrl = 'http://data.gov.tw/?q=data_list_json';
         $listFile = $cacheFolder . '/' . md5($listUrl);
@@ -14,13 +15,15 @@ class MainTask extends \Phalcon\CLI\Task {
         }
         $nodes = json_decode(substr(file_get_contents($listFile), 3)); //skip the first 3 characters, BOM
         $resultNodes = array();
+        $counter = 0;
         foreach ($nodes AS $node) {
             $nodeResult = array(
                 'title' => strip_tags($node->{'標題'}),
             );
             $quotePos = strpos($node->{'標題'}, '">');
             $nodeResult['link'] = 'http://data.gov.tw' . substr($node->{'標題'}, 9, $quotePos - 9);
-            $nodeResult['linkFile'] = $cacheFolder . '/' . md5($nodeResult['link']);
+            $fileName = md5($nodeResult['link']);
+            $nodeResult['linkFile'] = $cacheFolder . '/' . $fileName;
             if (!file_exists($nodeResult['linkFile'])) {
                 file_put_contents($nodeResult['linkFile'], file_get_contents($nodeResult['link']));
             }
@@ -44,6 +47,36 @@ class MainTask extends \Phalcon\CLI\Task {
                 $nextKey = $i + 1;
                 $nodeResult['dataUrl'] = substr($parts[$i], strrpos($parts[$i], '"') + 1);
                 $nodeResult['dataType'] = substr($parts[$nextKey], 0, strpos($parts[$nextKey], '"'));
+
+                ++$counter;
+                $dataFile = $dataFolder . '/' . $fileName . '_' . $nodeResult['dataType'];
+                if (!file_exists($dataFile)) {
+                    ;
+                    $request = new HttpRequest(urldecode('http://www.ylepb.gov.tw/df_ufiles/c/%E6%B0%B4%E5%BA%AB%E9%9B%86%E6%B0%B4%E5%8D%80.xls'), HttpRequest::METH_GET);
+                    //$request = new HttpRequest(urldecode($nodeResult['dataUrl']), HttpRequest::METH_GET);
+                    $request->setHeaders(array(
+                        'Accept-Language' => 'en-US,en;q=0.8',
+                        'Cache-Control' => 'no-cache',
+                        'Connection' => 'keep-alive',
+                        'Pragma' => 'no-cache',
+                        'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36',
+                    ));
+                    try {
+                        $message = $request->send();
+                    } catch (HttpException $ex) {
+                        //skip
+                    }
+                    file_put_contents($dataFile, $request->getResponseBody());
+                    $headerText = "source_url:{$nodeResult['link']}\n";
+                    $headerText .= "url:{$nodeResult['dataUrl']}\n";
+                    $headerText .= "code:{$request->getResponseCode()}\n";
+                    $headers = $message->getHeaders();
+                    foreach($headers AS $key => $val) {
+                        $headerText .= "{$key}:{$val}\n";
+                    }
+                    file_put_contents($dataFile . '_headers', $headerText);
+                }
+
                 $resultNodes[] = $nodeResult;
             }
         }
