2803a1751e9b6e747910bfad60f59bd41c42936a
kiang <kiange@gmail.com>
2014-01-17 02:18:12 +0800

    get values and dump to public file

--- app/tasks/RadiationTask.php
+++ app/tasks/RadiationTask.php
@@ -1,7 +1,7 @@
 <?php
 
 class RadiationTask extends \Phalcon\CLI\Task {
-    
+
     var $debug = true;
 
     public function mainAction() {
@@ -13,25 +13,50 @@ class RadiationTask extends \Phalcon\CLI\Task {
         $baseUrl = 'http://rmsp.trmc.aec.gov.tw/user/view_monitor.asp?c2e=&monitor_title=&monitor_no=';
         $points = array();
         for ($i = 1; $i <= 99; $i++) {
-            $url = $baseUrl . str_pad($i, 2, '0', STR_PAD_LEFT);
+            $key = str_pad($i, 2, '0', STR_PAD_LEFT);
+            $url = $baseUrl . $key;
             $localCachedFile = $dataFolder . '/' . $this->localFileName($url);
             $page = $this->getFileContent($localCachedFile, $url);
-            if(strlen($page) > 0) {
-                $points[$i] = array();
+            if (strlen($page) > 0) {
+                $points[$key] = array();
                 $page = mb_convert_encoding($page, 'utf-8', 'big5');
                 $pageParts = explode('<font size="3">', $page);
                 $pageParts = explode('</font>', $pageParts[1]);
-                $points[$i]['title'] = trim($pageParts[0]);
+                $points[$key]['title'] = trim($pageParts[0]);
                 $pageParts = explode('map.setCenter(new GLatLng(', $page);
                 $pageParts = explode('), 15);', $pageParts[1]);
                 $latlng = explode(',', $pageParts[0]);
-                $points[$i]['latitude'] = trim($latlng[0]);
-                $points[$i]['longitude'] = trim($latlng[1]);
+                $points[$key]['latitude'] = trim($latlng[0]);
+                $points[$key]['longitude'] = trim($latlng[1]);
             }
         }
-        print_r($points);
+
+        $valueSource = 'http://www.trmc.aec.gov.tw/utf8/showmap/taiwan_out.php';
+        $localCachedFile = $dataFolder . '/' . $this->localFileName($valueSource);
+        $map = $this->getFileContent($localCachedFile, $valueSource);
+        $offset = 0;
+        $jsPos = strpos($map, 'popupwindow(\'', $offset);
+        while (false !== $jsPos) {
+            $keyPosEnd = strpos($map, ',', $jsPos);
+            $key = substr($map, $jsPos + 13, 2);
+            if (!isset($points[$key])) {
+                $points[$key] = array();
+            }
+            $valuePosBegin = strpos($map, '<td align="right" class="blue13">', $jsPos);
+            $valuePosEnd = strpos($map, '</td>', $valuePosBegin) + 5;
+            $points[$key]['value'] = trim(strip_tags(substr($map, $valuePosBegin, $valuePosEnd - $valuePosBegin)));
+            //<span class="gray12"></span>
+            $valuePosBegin = strpos($map, '<span class="gray12">', $jsPos);
+            $valuePosEnd = strpos($map, '</span>', $valuePosBegin) + 7;
+            $points[$key]['date'] = trim(strip_tags(substr($map, $valuePosBegin, $valuePosEnd - $valuePosBegin)));
+
+            //next loop
+            $offset = $valuePosEnd;
+            $jsPos = strpos($map, 'popupwindow(\'', $offset);
+        }
+        file_put_contents(dirname(APPLICATION_PATH) . '/public/files/trmc.aec.gov.tw.json', json_encode($points));
     }
-    
+
     private function getFileContent($localFile, $remoteFile) {
         if (!file_exists($localFile)) {
             if (true === $this->debug) {
@@ -49,7 +74,7 @@ class RadiationTask extends \Phalcon\CLI\Task {
         }
         return $fileName;
     }
-    
+
     private function fixUrl($url) {
         //ref: http://ubuntu-rubyonrails.blogspot.tw/2009/06/unicode.html
         preg_match_all('/[\x{2E80}-\x{9FFF}]+/u', $url, $matches, PREG_OFFSET_CAPTURE);
